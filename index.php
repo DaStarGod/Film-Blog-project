89e2a6d4803c95dcae83055f00b07cc80e616c02
jonathan patelski <Jonathanpatelski@gmail.com>
2023-03-16 14:59:00 +0100

    html naar php veranderd

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <nav class="bg-slate-600 shadow-sm">
+        <ul class="flex">
+            <li class="mr-6 text-center">
+              <a class="text-slate-400 hover:text-slate-50" href="index.html">Home</a>
+            </li>
+            <li class="mr-6 text-right">
+                <a class="text-slate-400 hover:text-slate-50" href="index.html">Admin Panel</a>
+            </li>
+          </ul>
+    </nav>
+
+    <main class="p-12 bg-slate-700 min-h-screen">
+        <div class="container mx-auto px-4 flex">
+            <div class="w-3/12 bg-slate-600 py-12"></div>
+            <div class="w-6/12 bg-slate-600 py-12 mx-6"></div>
+            <div class="w-3/12 bg-slate-600 py-12"></div>
+        </div>
+    </main>
+</body>
+</html>
