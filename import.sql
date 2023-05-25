506ae3c53f9c36bee249b62a0a027fd74528eb61
jonathan patelski <Jonathanpatelski@gmail.com>
2023-05-25 14:17:59 +0200

    Database structure veranderd

--- import.sql
+++ import.sql
@@ -1,16 +1,29 @@
-CREATE DATABASE blog;
+CREATE DATABASE Movie_reviews;
 
-CREATE TABLE Posts (
-    id int,
-    titel varchar(255),
-    body longtext,
-    auteur varchar(255),
-    category_id int,
-    published_at varchar(255)
+CREATE TABLE movies (
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    release_year INT,
+    genre VARCHAR(255),
+    director VARCHAR(255),
+    description TEXT
 );
 
-CREATE TABLE categories (
-    id int,
-    naam varchar(255)
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE reviews (
+    id INT PRIMARY KEY,
+    movie_id INT,
+    user_id INT,
+    rating DECIMAL(2,1),
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (movie_id) REFERENCES movies(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
